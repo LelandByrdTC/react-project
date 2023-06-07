8e1623b73a913ab102feacfdaae3bfba938d3dad
Leland Byrd <lbyrd@truecoders.io>
2023-06-07 14:28:29 -0500

    created react app basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,25 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1>REACT AMAZINGNESSssssss</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          React is so cool
         </p>
+        <p> Facebook made it!</p>
+        <ul>
+          <li>Zuckerberg</li>
+          <li>cannot be </li>
+          <li>over styled</li>
+        </ul>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          React Docs
         </a>
       </header>
     </div>
